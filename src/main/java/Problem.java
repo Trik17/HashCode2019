3d3d3929a8be560411c2dbd790f900925b46f756
Trik17 <andreatricarico17@gmail.com>
2019-02-24 12:40:01 -0600

    input

--- src/main/java/Problem.java
+++ src/main/java/Problem.java
@@ -1,10 +1,20 @@
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public abstract class Problem {
 
     public String path;
     public String input;
 
     public void read(){
-
+        try
+        {
+            input  = new String(Files.readAllBytes(Paths.get(path)));
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
     public abstract void setupProblem();
