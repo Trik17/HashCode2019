101f6e01c7a5b3c71211566799049611b38dfb00
Filippo Rezzonico <rabbin95@gmail.com>
2019-02-28 12:44:17 -0600

    print

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,24 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
     public static void main (String args[]){
-        Problem prob = new Problem();
-
-        prob.read();
-        System.out.println("Hello");
+        //Problem prob = new Problem();
+        List<Slide> ss = new ArrayList<Slide>();
+        Photo p1 = new Photo();
+        Slide s1 = new Slide(p1);
+        ss.add(s1);
+        /*prob.read();
+        //System.out.println("Hello");
         List<Photo> collection = prob.pictures;
-        SlideShow ss = new SlideShow();
+
+        //SlideShow ss = new SlideShow();
         Photo verticalP = null;
         for(Photo picture : collection){
             if(!picture.isVertical){
                 Slide s = new Slide(picture);
-                ss.slides.add(s);
+                ss.add(s);
             }
             else{
                 if(verticalP == null){
@@ -21,9 +26,21 @@ public class Main {
                 }
                 else {
                     Slide s = new Slide(verticalP, picture);
-                    ss.slides.add(s);
+                    ss.add(s);
                 }
             }
+        }*/
+        String submissionString;
+        submissionString = ss.size() + "\n";
+        for(Slide slide : ss){
+            if(!slide.isVertical) {
+                submissionString += slide.pic1.id + "\n";
+            }
+            else{
+                submissionString += slide.pic1.id + " " + slide.pic2.id + "\n";
+            }
+            System.out.println(submissionString);
+
         }
 
 
