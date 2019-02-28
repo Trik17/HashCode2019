ad37bf28324aea50ca3d6b4db7fa8ce27da67a3c
Filippo Rezzonico <rabbin95@gmail.com>
2019-02-28 12:20:46 -0600

    c

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,18 @@
+import java.util.List;
+
 public class Main {
 
     public static void main (String args[]){
         Problem prob = new Problem();
 
         prob.read();
-
+        System.out.println("Hello");
+        List<Photo> collection = prob.pictures;
+        List<Slide> slideShow;
+        for(Photo picture : collection){
+            Photo verticalP;
+            if(!picture.isVertical())
+        }
 
         
 
