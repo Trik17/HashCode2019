2d36a95586d2fd34e9db875a16d48f7f71b704fa
Trik17 <andreatricarico17@gmail.com>
2019-02-28 13:45:00 -0600

    finito photo functions

--- src/main/java/Photo.java
+++ src/main/java/Photo.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.util.HashSet;
 
 public class Photo {
@@ -7,21 +8,33 @@ public class Photo {
     public boolean isVertical;
     public HashSet<String > tags= new HashSet<>();
 
-    public Photo(int id, boolean isVertical, HashSet<String > tags){
+    public Photo(int id, boolean isVertical, HashSet<String> tags){
         this.id=id;
         this.isVertical=isVertical;
         this.tags=tags;
     }
 
     public static int union(Photo p1, Photo p2){
-        //todo
-        // ritorna il numeor degli elementi dei tag delle 2 foto
-        return 0;
+        // ritorna il numero  dei tag dell'unione delle 2 foto
+        HashSet<String> res = new HashSet<>();
+        res.addAll(p1.tags);
+        res.addAll(p2.tags);
+        return res.size();
     }
 
     @Override
     public boolean equals(Object obj) {
-        return super.equals(obj);
-        //todo
+        if (this.id == ((Photo) obj).id) {
+            /*if (this.value != ((Photo) obj).value){
+                try {
+                    throw new Exception();
+                } catch (Exception e) {
+                    System.out.println("There are two instances of a cell with different values!");
+                    e.printStackTrace();
+                }
+            }*/
+            return true;
+        }
+        return false;
     }
 }
